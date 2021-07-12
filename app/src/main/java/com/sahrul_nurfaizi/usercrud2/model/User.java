c82c838c63683b91b938a856645e6501075e8613
cloningsn29 <sahrulnurfaizi6@gmail.com>
2021-07-12 17:07:23 +0700

    Userapi2

---
+++ app/src/main/java/com/sahrul_nurfaizi/usercrud2/model/User.java
@@ -0,0 +1,44 @@
+package com.sahrul_nurfaizi.usercrud2.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+    @SerializedName("id")
+    private String id;
+    @SerializedName("nama")
+    private String nama;
+    @SerializedName("email")
+    private  String email;
+
+    public User(){}
+    public User(String id, String nama, String email){
+        this.id = id;
+        this.nama = nama;
+        this.email = email;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
+
